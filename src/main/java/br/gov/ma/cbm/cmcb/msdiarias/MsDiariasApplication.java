878ff3d567b982e45c9988d595ca0547faf3402b
Unidade I <dinizdcmcb@gmail.com>
2021-02-05 11:16:17 -0300

    Criaçao do ms-diarias

---
+++ src/main/java/br/gov/ma/cbm/cmcb/msdiarias/MsDiariasApplication.java
@@ -0,0 +1,13 @@
+package br.gov.ma.cbm.cmcb.msdiarias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDiariasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDiariasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/ma/cbm/cmcb/msdiarias/MsDiariasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.ma.cbm.cmcb.msdiarias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDiariasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
